477772a35487163876774a1d2e3ea03bb86425d6
xiaoming <xiaoming@qq.com>
2018-11-14 19:43:00 +0800

    添加了一个bug

--- src/main/java/com/yyq/gitTest/firstgit.java
+++ src/main/java/com/yyq/gitTest/firstgit.java
@@ -8,5 +8,7 @@ public class firstgit {
     public static void main(String[] args) {
         System.out.println("实现第一个github");
         System.out.println("hello world!");
+        int i = 10/0;
+        System.out.println(i);
     }
 }
