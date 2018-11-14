f3c5911eeb8498da12b3fc4cd2e76cc257cea962
xiaoming <xiaoming@qq.com>
2018-11-14 17:36:21 +0800

    Initial commit

---
+++ src/main/java/com/yyq/gitTest/firstgit.java
@@ -0,0 +1,11 @@
+package com.yyq.gitTest;
+
+public class firstgit {
+    /**
+     * 第一次添加
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("实现第一个github");
+    }
+}
